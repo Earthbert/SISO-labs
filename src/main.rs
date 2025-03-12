d05d580209fd17fb5cbe23f0684616419dfaee08
Earthbert <daraban_timotei@tutanota.com>
2025-03-12 12:51:01 +0200

    Init commit
    Signed-off-by: Earthbert <daraban_timotei@tutanota.com>

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+use vga_buffer::print_something;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    print_something();
+
+    loop {}
+}
