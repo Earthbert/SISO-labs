82878c807272646299908e3f2236a2ee259c4c9d
Earthbert <daraban_timotei@tutanota.com>
2025-03-17 14:05:36 +0200

    Sure
    Signed-off-by: Earthbert <daraban_timotei@tutanota.com>

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
+// in src/main.rs
+
 #![no_std]
 #![no_main]
-
-mod vga_buffer;
+#![feature(custom_test_frameworks)]
+#![test_runner(siso::test_runner)]
 
 use core::panic::PanicInfo;
-
-use vga_buffer::print_something;
-
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+use siso::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    print_something();
-
+    siso::init();
+    println!("Hello Kernal!");
     loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    siso::test_panic_handler(info)
 }
